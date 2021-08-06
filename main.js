7f522a82149759d530c8d8e9d5d087ca9165a762
ManitVarma <78251297+ManitVarma@users.noreply.github.com>
2021-08-06 19:48:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.position(310, 270);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("myFilteredImage.png");
+}
